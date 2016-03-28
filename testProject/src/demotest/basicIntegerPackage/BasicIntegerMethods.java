8e876e32f452c9774e9ff3ed142a876d233e682f
Muntazir Fadhel <muntazir@live.ca>
2016-03-28 08:46:56 -0400

    initial baseline

---
+++ testProject/src/demotest/basicIntegerPackage/BasicIntegerMethods.java
@@ -0,0 +1,29 @@
+package demotest.basicIntegerPackage;
+
+public class BasicIntegerMethods {
+	
+	public BasicIntegerMethods(){
+		
+	}
+	
+	public int addition(int x, int y){
+		
+		int sum = 0;
+		
+		sum = x + y;
+	
+		return sum;
+	}
+	
+	public int multiply(int x, int y){
+		
+		int product;
+		
+		product = x * y;
+		
+		return product;
+		
+	}
+	
+	
+}
