7a043ec93d4969a4020db74aaa047e92a1ad8a07
Muntazir Fadhel <muntazir@live.ca>
2016-03-28 10:01:57 -0400

    lolcakes.

---
+++ testProject/src/demotest/TestGridDemo.java
@@ -0,0 +1,17 @@
+package demotest;
+
+public class TestGridDemo {
+
+    public TestGridDemo() {
+
+    }
+
+    public boolean reactorLevelTooHigh(int reactorLevel1, int reactorLevel2) {
+
+        if (((reactorLevel1 + reactorLevel2) / 2) > 50) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
