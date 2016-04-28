bd89f6424a6b6d0ad4c6347951e14caacde92691
Muntazir Fadhel <muntazir@live.ca>
2016-04-28 08:50:31 -0400

    pressure demo

--- testProject/src/demotest/complexIntegerPackage/PressureDemo.java
+++ testProject/src/demotest/complexIntegerPackage/PressureDemo.java
@@ -1,15 +1,26 @@
 package demotest.complexIntegerPackage;
 
-/**
- * Calculates the pressure in a system with with two sensors
- * 
- * @author Muntazir Fadhel
- */
-public class PressureDemo {
+class PressureDemo {
 
-    public static int calculatePressure(int s1, int s2, boolean emergStop) {
-
-        return 1;
+    public int calculatePressure(int s1, int s2, boolean emergStop) {
+        if (emergStop) {
+            return -1;
+        }
+        else {
+            if (Math.abs(s1 - s2) > 5) {
+                return -2;
+            }
+            else {
+                if ((s1 + s2) / 2 < 40) {
+                    return -3;
+                }
+ else if (((double) s1 + (double) s2) / 2 > 80) {
+                    return -4;
+                }
+                else {
+                    return (s1 + s2) / 2;
+                }
+            }
+        }
     }
-
 }
